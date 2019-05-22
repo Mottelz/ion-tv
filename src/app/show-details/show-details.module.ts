6cd639761ad66f0d42bac42c7946d152efe435d5
Mottel Zirkind <shachna@gmail.com>
2019-05-22 15:05:38 -0400

    Displaying episodes as list. Using a simple pipe to help with number formatting.

--- src/app/show-details/show-details.module.ts
+++ src/app/show-details/show-details.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
 import { IonicModule } from '@ionic/angular';
-
 import { ShowDetailsPage } from './show-details.page';
+import {EpisodeNumberPrettierPipe} from '../pipes/episode-number-prettier.pipe';
 
 const routes: Routes = [
   {
@@ -15,12 +14,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
-  declarations: [ShowDetailsPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule.forChild(routes)
+    ],
+  declarations: [ShowDetailsPage, EpisodeNumberPrettierPipe]
 })
 export class ShowDetailsPageModule {}
